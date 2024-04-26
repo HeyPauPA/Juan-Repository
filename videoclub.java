7d6649c940d18061ec14c60a2ac1833c2df9216b
HeyPauPA <131009562+HeyPauPA@users.noreply.github.com>
2024-04-26 14:09:46 +0200

    Add files via upload

---
+++ videoclub.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class videoclub {
+    public List<Pelicula> película = new ArrayList<Pelicula> ();
+
+    public void alquilar(final int pelicula) {
+    }
+
+    public void devolver(final int pelicula) {
+    }
+
+}
